6d85dbe13bc56dd20e980b3e601f9819f4718d1a
Mark Anthony <touya1313@hotmail.com>
2023-10-18 02:01:04 -0600

    finished inquirer with help from prev activities and docs

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Here I will collect user input using inquirer. 
+ // First, some connections
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
+
+// Here I will collect user input using inquirer.
+async function getUserInput() {
+    const userInput = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter three characters for your logo:',
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a color for the text color:',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Select the shape for your logo:',
+            choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter a color for the shape color:',
+        },
+    ]);
+
+    return userInput;
+}
