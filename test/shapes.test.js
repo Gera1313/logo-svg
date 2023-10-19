e27d909f923eb1d6552d0a5ed6447483b014f5e4
Mark Anthony <touya1313@hotmail.com>
2023-10-19 01:04:00 -0600

    had to reinstall nodemodules and dependencies. first test passed

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -4,4 +4,22 @@ const Circle = require('../lib/circle');
 const Triangle = require('../lib/triangle');
 const Square = require('../lib/square');
 
+test('Circle render() method should return the correct SVG string', () => {
+    // Test code for Circle class
+    const circle = new Circle('red', 50);
 
+    const result = circle.render();
+
+    expect(result).toContain('<circle');
+    expect(result).toContain('fill="red"');
+    expect(result).toContain('r="50"');
+  });
+  
+  test('Triangle render() method should return the correct SVG string', () => {
+    // Test code for Triangle class
+  });
+  
+  test('Square render() method should return the correct SVG string', () => {
+    // Test code for Square class
+  });
+  
